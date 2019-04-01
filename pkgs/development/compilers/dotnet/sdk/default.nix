cef52d093f6ad4e476474e1269d436df2436abaa
lippirk <benanson5@gmail.com>
2019-04-01 20:13:09 +0100

    dotnetcore 3.0.0

--- pkgs/development/compilers/dotnet/sdk/default.nix
+++ pkgs/development/compilers/dotnet/sdk/default.nix
@@ -12,12 +12,12 @@ let
   rpath = stdenv.lib.makeLibraryPath [ stdenv.cc.cc libunwind libuuid icu openssl zlib curl ];
 in
   stdenv.mkDerivation rec {
-    version = "2.1.302";
+    version = "3.0.100-preview3-010431";
     name = "dotnet-sdk-${version}";
 
     src = fetchurl {
       url = "https://dotnetcli.azureedge.net/dotnet/Sdk/${version}/dotnet-sdk-${version}-linux-x64.tar.gz";
-      sha256 = "1a8z9q69cd9a33j7fr7907abm5z4qiivw5k379cgsjmmvxwyvjia";
+      sha512 ="EAA1208590E60A793CAA8A35E9A26722A4E3BCD8733EA1F79D18C7A777E1A5A8E9D82EDDABBD18B82D8795838E6DFC3B5EC7C4504E58C0C852F2D69D40131CFE";
     };
 
     unpackPhase = ''
@@ -48,7 +48,7 @@ in
 
     meta = with stdenv.lib; {
       homepage = https://dotnet.github.io/;
-      description = ".NET Core SDK 2.0.2 with .NET Core 2.0.0";
+      description = ".NET Core SDK 3.0.0 with .NET Core 3.0.0";
       platforms = [ "x86_64-linux" ];
       maintainers = with maintainers; [ kuznero ];
       license = licenses.mit;
